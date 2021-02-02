2e1d45be3db922945a5f231af6f87babeb0b8aab
thomasguillaux <thomasguillaux@gmail.com>
2021-02-02 18:19:43 +0100

    mods

--- src/Highlight.js
+++ src/Highlight.js
@@ -4,8 +4,19 @@ import movietitle from './assets/movie-title.webp'
 const Highlight = () => {
     return (
         <div className="highlight">
-            <img className="bg-picture" src={moviepicture} alt="bg"/>
-            <img className="title-picture" src={movietitle} alt="title"/>
+            <div className="bgPicture">
+            <img src={moviepicture} alt="bg"/>
+                <div className="titlePicture">
+                <img src={movietitle} alt="title"/>
+                <p>A brutal rape and a violent revenge, told in reverse. This time, there's no escaping the worst of human nature</p>
+                <div className="button">
+                    <button className="play">Play</button>
+                    <button className="info">More info</button>
+                </div>
+                </div>
+            </div>
+            
+           
         </div>
     )
 }
