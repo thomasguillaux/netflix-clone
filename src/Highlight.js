1b4a34fe282bdab359cb1c9a5bfaec6339562c41
thomasguillaux <thomasguillaux@gmail.com>
2021-02-02 18:48:52 +0100

    buttons

--- src/Highlight.js
+++ src/Highlight.js
@@ -1,5 +1,7 @@
 import moviepicture from './assets/movie-picture.webp'
 import movietitle from './assets/movie-title.webp'
+import { FaPlay } from "react-icons/fa";
+import { FaInfoCircle } from "react-icons/fa";
 
 const Highlight = () => {
     return (
@@ -10,8 +12,8 @@ const Highlight = () => {
                 <img src={movietitle} alt="title"/>
                 <p>A brutal rape and a violent revenge, told in reverse. This time, there's no escaping the worst of human nature</p>
                 <div className="button">
-                    <button className="play">Play</button>
-                    <button className="info">More info</button>
+                    <button className="play"><span><FaPlay/></span> <span>Play</span> </button>
+                    <button className="info"><span><FaInfoCircle/></span> <span>More info</span> </button>
                 </div>
                 </div>
             </div>
